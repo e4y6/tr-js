c55699ba09b690bfbd7b2249a679f6fc96f309d1
e4y6 <tchehy9@gmail.com>
2025-02-27 23:23:08 +0200

    modified:   js/lsn1.js

--- js/lsn1.js
+++ js/lsn1.js
@@ -1,5 +1,7 @@
-function checkForSpam(message) {
-   return message.toLowerCase().includes('spam') || message.toLowerCase().includes('spam')
-}
+// function checkForSpam(message) {
+//    return message.toLowerCase().includes('spam') || message.toLowerCase().includes('spam')
+// }
+
+const checkForSpam=message=> message.toLowerCase().includes('spam') || message.toLowerCase().includes('spam')
 
 console.log(checkForSpam("Trust me, this is not a sPam message"))
