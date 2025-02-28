bd4420b38a97b33ce314f49ce9bfd39c5eb27ba1
e4y6 <tchehy9@gmail.com>
2025-02-28 20:03:31 +0200

    modified:   js/lsn1.js

--- js/lsn1.js
+++ js/lsn1.js
@@ -12,3 +12,26 @@ console.log(words)
 }
 writeWords('That`s it.')
 }
+  
+{
+//   function NameMe(first, last) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     return {name: this.firstName + ' ' + this.lastName};
+// }
+
+  class NameMe {
+    constructor(first, last) {
+      this.firstName = first;
+      this.lastName = last;
+      return { name: this.firstName + ' ' + this.lastName };
+    }
+  }
+
+  var n = new NameMe('John', 'Doe');
+  n.firstName //Expected: John
+  n.lastName //Expected: Doe
+  n.name //Expected: John Doe
+
+  console.log(n.name)
+  }
