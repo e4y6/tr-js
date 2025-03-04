31f3ec690122fd24781148c3268438a5da563381
e4y6 <tchehy9@gmail.com>
2025-03-04 17:28:28 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -169,5 +169,48 @@ Examples
   const capMe = (names) =>
     names.map((e) => e.toLowerCase().replace(/./, (w) => w.toUpperCase()));
 
-  console.log(capMe(["RALPH", "GEORGIA", "CHRISTINA", "po"]));
+  // console.log(capMe(["RALPH", "GEORGIA", "CHRISTINA", "po"]));
+}
+
+{
+  /** Coding in function countGrade. function accept 1 parameters scores, it's a number array. Your task is to count the grade distribution of the scores, to return an object like this:
+
+{S:888, A:888, B:888, C:888, D:888, X:888}
+Grading rules:
+
+Grade S: Full marks(score=100)
+Grade A: score<100 and score>=90
+Grade B: score<90 and score>=80
+Grade C: score<80 and score>=60
+Grade D: score<60 and score>=0
+Grade X: score=-1(The cheating guy gets a score like that)
+Example
+countGrade([50,60,70,80,90,100]) should return {S:1, A:1, B:1, C:2, D:1, X:0}
+countGrade([65,75,,85,85,95,100,100]) should return {S:2, A:1, B:2, C:2, D:0, X:0}
+countGrade([-1,-1,-1,-1,-1,-1]) sho */
+
+  const countGrade = (scores) => {
+    const keys = ["S", "A", "B", "C", "D", "X"];
+    let obj = {};
+    keys.map((e) => {
+      if (e === "S")
+        obj = { ...obj, S: scores.filter((sc) => sc === 100).length };
+      if (e === "A")
+        obj = {
+          ...obj,
+          A: scores.filter((sc) => sc >= 90 && sc < 100).length,
+        };
+      if (e === "B")
+        obj = { ...obj, B: scores.filter((sc) => sc >= 80 && sc < 90).length };
+      if (e === "C")
+        obj = { ...obj, C: scores.filter((sc) => sc >= 60 && sc < 80).length };
+      if (e === "D")
+        obj = { ...obj, D: scores.filter((sc) => sc >= 0 && sc < 60).length };
+      if (e === "X")
+        obj = { ...obj, X: scores.filter((sc) => sc === -1).length };
+    });
+
+    return obj;
+  };
+  console.log(countGrade([50, 60, 70, 80, 90, 100]));
 }
