56b6e732540ed795a4d5ab56a091cb555dc832ec
e4y6 <tchehy9@gmail.com>
2025-03-04 18:43:55 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -222,5 +222,23 @@ countGrade([-1,-1,-1,-1,-1,-1]) sho */
     X: scores.filter((sc) => sc === -1).length,
   });
 
-  console.log(countGrade([50, 60, 70, 80, 90, 100]));
+  // console.log(countGrade([50, 60, 70, 80, 90, 100]));
+}
+
+{
+  /** Improving Math.round(x)
+  Javascript has a handy built-in Math.round(x) method that rounds a number to the nearest integer:
+  But that is sometimes not enough! What if we wanted to round 3.1415926535 to two decimal places?
+   While some languages (such as PHP) has a round() function that supports rounding to a specified
+    number of decimal places (e.g. <?php round(3.1415926535, 4); /* 3.1416 (rounded to 4 d.p. 
+    as specified)  ?>), Javascript does not!
+  
+  Task
+  Your task is to create a method, Math.roundTo(number, precision) which rounds a given number to precision decimal places. */
+
+  Math.roundTo = function (number, precision) {
+    return Number(number.toFixed(precision));
+  };
+
+  console.log(Math.roundTo(3.1415926535, 4));
 }
