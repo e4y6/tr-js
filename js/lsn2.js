ee2bd5813cd8c152d7680dec39697cf43465cdbf
e4y6 <tchehy9@gmail.com>
2025-03-05 17:14:07 +0200

    modified:   js/lsn2.js

--- js/lsn2.js
+++ js/lsn2.js
@@ -1,16 +1,49 @@
-function checkAge(age) {
-  if (age >= 18) {
-    return "You are an adult";
+{
+  function checkAge(age) {
+    if (age >= 18) {
+      return "You are an adult";
+    }
+
+    return "You are a minor";
   }
 
-  return "You are a minor";
-}
+  function checkPassword(password) {
+    const ADMIN_PASSWORD = "jqueryismyjam";
 
-function checkPassword(password) {
-  const ADMIN_PASSWORD = "jqueryismyjam";
-
-  if (password === ADMIN_PASSWORD) {
-    return "Welcome!";
+    if (password === ADMIN_PASSWORD) {
+      return "Welcome!";
+    }
+    return "Access denied, wrong password!";
   }
-  return "Access denied, wrong password!";
+
+  function checkStorage(available, ordered) {
+    if (ordered === 0) {
+      return "Your order is empty!";
+    }
+    if (ordered > available) {
+      return "Your order is too large, not enough goods in stock!";
+    }
+    return "The order is accepted, our manager will contact you";
+  }
+
+  const fruits = ["apple", "plum", "pear", "orange"];
+  const [firstElement, secondElement] = fruits;
+  let lastElement = fruits[fruits.length - 1];
+  // console.log(firstElement);
+
+  fruits[1] = "peach";
+  fruits[3] = "banana";
+
+  const fruitsArrayLength = fruits.length;
+  const lastElementIndex = fruits.length - 1;
+  lastElement = fruits[lastElementIndex];
+  // console.log(fruits);
+
+  function getExtremeElements(array) {
+    return [array[0], array[array.length - 1]];
+  }
+
+  function splitMessage(message, delimiter) {
+    return message.split(delimiter);
+  }
 }
