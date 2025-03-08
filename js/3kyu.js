9cd9ad02798b2d02119db1424c25ca7d980e533b
e4y6 <tchehy9@gmail.com>
2025-03-08 23:04:13 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -252,7 +252,14 @@ countGrade([-1,-1,-1,-1,-1,-1]) sho */
 
   // console.log(sumSquares([11, 13, 15, 18, 2]));
 
-  /**   */
+  /** 7 kyu  Sum - Square Even, Root Odd
+   * Complete the function that takes a list of numbers (nums), as the only argument
+   *  to the function. Take each number in the list and square it if it is even,
+   *  or square root the number if it is odd. Take this new list and return the sum 
+   * of it, rounded to two decimal places.
+
+The list will never be empty and will only contain values that are greater
+ than or equal to zero.   */
 
   // const sumSquareEvenRootOdd = (ns) => {
   //   let sum = 0;
@@ -298,4 +305,8 @@ countGrade([-1,-1,-1,-1,-1,-1]) sho */
       .toFixed(2);
 
   // console.log(sumSquareEvenRootOdd([4, 5, 7, 8, 1, 2, 3, 0]));
+
+  const remove = (string) => string.replace(/!+(\s*)(\w)/g, "$1$2");
+  console.log(remove("Hi! Hi!", "Hi Hi!"));
+  console.log(remove("!Hi!", "Hi Hi!"));
 }
