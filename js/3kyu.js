b6a3494e9b854ba1c4f0779c4818ee2ff6bc1828
e4y6 <tchehy9@gmail.com>
2025-03-12 18:41:59 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -310,3 +310,66 @@ The list will never be empty and will only contain values that are greater
   // console.log(remove("Hi! Hi!"));
   // console.log(remove("!Hi!"));
 }
+
+{
+  /** Coding in function blackAndWhite. function accept 1 parameter arr(a number array).
+
+  If arr is not an array, function should return:
+  
+  "It's a fake array"
+  If arr contains elements 5 and 13, function should return:
+  
+  "It's a black array"
+  If arr contains neither 5 nor 13, function should return:
+  
+  "It's a white array"
+  Examples
+  blackAndWhite(5,13) should return "It's a fake array"
+  blackAndWhite([5,13]) should return "It's a black array"
+  blackAndWhite([5,12]) should return " */
+
+  // function blackAndWhite(arr) {
+  //   if (!Array.isArray(arr)) {
+  //     return "It's a fake array";
+  //   }
+  //   if (arr.includes(5) && arr.includes(13)) {
+  //     return "It's a black array";
+  //   }
+  //   return "It's a white array";
+  // }
+
+  // function blackAndWhite(arr) {
+  //   return !Array.isArray(arr)
+  //     ? "It's a fake array"
+  //     : arr.includes(5) && arr.includes(13)
+  //     ? "It's a black array"
+  //     : "It's a white array";
+  // }
+
+  // const blackAndWhite = (arr) =>
+  //   !Array.isArray(arr)
+  //     ? "It's a fake array"
+  //     : arr.includes(5) && arr.includes(13)
+  //     ? "It's a black array"
+  //     : "It's a white array";
+
+  // const blackAndWhite = (arr) =>
+  //   `It's a ${
+  //     !Array.isArray(arr)
+  //       ? `fake`
+  //       : arr.includes(5) && arr.includes(13)
+  //       ? `black`
+  //       : `white`
+  //   } array`;
+
+  const blackAndWhite = (arr) =>
+    "It's a " +
+    (!Array.isArray(arr)
+      ? "fake"
+      : arr.includes(5) && arr.includes(13)
+      ? `black`
+      : `white`) +
+    " array";
+
+  // console.log(blackAndWhite([5, 13]));
+}
