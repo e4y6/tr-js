2dc652ac57b1f81c17dd5f0e27883ccc65869839
e4y6 <tchehy9@gmail.com>
2025-03-13 17:34:55 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -373,3 +373,17 @@ The list will never be empty and will only contain values that are greater
 
   // console.log(blackAndWhite([5, 13]));
 }
+{
+  /**  8 kyu  Simple Comparison?
+Write a function that will compare two values, one will be a number and one will be
+ a string. Return true if they are the same character (regardless of their different
+  data types) and return false if they are not.
+
+To make this challange harder and to promp the challenger to read up about coercion
+ I have disabled some of the built in methods including .toString(), .join(), .split(),
+  parseInt and .Number().*/
+
+  function add(a, b) {
+    return +a === +b;
+  }
+}
