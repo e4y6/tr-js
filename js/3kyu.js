faf965a9bdd636b5fa92c5f043b01326d2adaa2b
e4y6 <tchehy9@gmail.com>
2025-04-14 13:20:29 +0000

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -447,3 +447,38 @@ You will get an array as input with time durations as string in the following fo
 
   // console.log(determineTime(["04:30:00", "02:00:00", "01:30:00", "16:00:00"]));
 }
+
+{
+  /**   You get a list of integers, and you have to write a function mirror that returns the "mirror" (or symmetric) version of this list: i.e. the middle element is the greatest, then the next greatest on both sides, then the next greatest, and so on...
+  
+  The list will always consist of integers in range -1000..1000 and will vary in size between 0 and 10000. Your function should not mutate the input array, and this will be tested (where applicable). Notice that the returned list will always be of odd size, since there will always be a definitive middle element.
+  Examples
+  
+  []  -->  []
+  [1]  -->  [1]
+  [2, 1]  -->  [1, 2, 1]
+  [1, 3, 2]  -->  [1, 2, 3, 2, 1]
+  [-8, 42, 18, 0, -16]  -->  [-16, -8, 0, 18, 42, 18, 0, -8, -16]
+  [-3, 15, 8, -1, 7, -1] --> [-3, -1, -1, 7, 8, 15, 8, 7, -1, -1, -3]
+  [-5, 10, 8, 10, 2, -3, 10] --> [-5, -3, 2, 8, 10, 10, 10, 10, 10, 8, 2, -3, -5]   */
+
+  /** Test failed: Expected values to be loosely deep-equal: */
+
+  function mirror(data) {
+    const length = data.length;
+
+    if (!length) return [];
+
+    let max = Math.max(...data);
+    let arr = [...data];
+    let newArr = [];
+    for (let index = 0; index < length; index++) {
+      max = Math.max(...arr);
+      newArr = [max, ...newArr, max];
+      arr = arr.filter((e) => e - max);
+    }
+    newArr.splice(length - 1, 1);
+    return newArr;
+  }
+  // console.log(mirror([-8, 42, 18, 0, -16]));
+}
