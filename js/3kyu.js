b6776d48f2a9b99146a92c88970daccfa7aa1264
e4y6 <tchehy9@gmail.com>
2025-10-29 23:06:35 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -639,9 +639,26 @@ tCopy = [1, 2, 3, 4]
 }
 
 {
-  function createArrayOfTiers(num) {
-    return [...(num + "")].map((e, i) => (num + "").slice(0, i + 1));
-  }
+  /**  
+7 kyu
+Number to digit tiers
+Create a function that takes a number and returns an array of strings containing the number cut off at each digit.
+Examples
+
+    420 should return ["4", "42", "420"]
+    2017 should return ["2", "20", "201", "2017"]
+    2010 should return ["2", "20", "201", "2010"]
+    4020 should return ["4", "40", "402", "4020"]
+    80200 should return ["8", "80", "802", "8020", "80200"]
+
+PS: The input is guaranteed to be an integer in the range [0, 1000000]
+ */
+  // function createArrayOfTiers(num) {
+  //   return [...(num + "")].map((e, i) => (num + "").slice(0, i + 1));
+  // }
+
+  const createArrayOfTiers = (num) =>
+    [...(num + "")].map((e, i) => (num + "").slice(0, i + 1));
 
   console.log(createArrayOfTiers(768));
 }
