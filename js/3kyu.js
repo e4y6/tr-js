2e18e873b689077e7a1a099d2e5cc3370197f215
e4y6 <tchehy9@gmail.com>
2025-11-05 21:30:44 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -666,3 +666,19 @@ PS: The input is guaranteed to be an integer in the range [0, 1000000]
 
   // console.log(createArrayOfTiers(768));
 }
+
+{
+  /**  7 kyu    Unpacking Arguments
+ 
+You must create a function, spread, that takes a function and a list of arguments to be applied to that function. You must make this function return the result of calling the given function/lambda with the given arguments.
+
+eg:
+
+spread(someFunction, [1, true, "Foo", "bar"] ) 
+// is the same as...
+someFunction(1, true, "Foo", "bar")    */
+
+  function spread(func, args) {
+    return func(...args);
+  }
+}
