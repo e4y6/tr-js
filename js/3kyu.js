f36e3e68f4ba3ec07e2b8eed4205c537a345cc7f
e4y6 <tchehy9@gmail.com>
2025-11-18 22:13:10 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -875,3 +875,118 @@ const forbidden = "
 
   // console.log(reverse(1234));
 }
+{
+  /**  7 kyu    Scrabble Score
+Write a program that, given a word, computes the scrabble score for that word.
+Letter Values
+
+You'll need these:
+
+Letter                           Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+
+There will be a preloaded hashtable $dict with all these values: $dict["E"] == 1.
+Examples
+
+"cabbage" --> 14
+
+"cabbage" should be scored as worth 14 points:
+
+    3 points for C
+    1 point for A, twice
+    3 points for B, twice
+    2 points for G
+    1 point for E
+
+And to total:
+
+3 + 2*1 + 2*3 + 2 + 1 = 3 + 2 + 6 +
+7 kyu
+Scrabble Score
+711891% of 6532,055 of 4,834noku1 Issue Reported
+
+    JavaScript
+    Node v18.x
+
+        VIM
+        EMACS
+
+Instructions
+Output
+Past Solutions
+
+Write a program that, given a word, computes the scrabble score for that word.
+Letter Values
+
+You'll need these:
+
+Letter                           Value
+A, E, I, O, U, L, N, R, S, T       1
+D, G                               2
+B, C, M, P                         3
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+
+There will be a preloaded hashtable $dict with all these values: $dict["E"] == 1.
+Examples
+
+"cabbage" --> 14
+
+"cabbage" should be scored as worth 14 points:
+
+    3 points for C
+    1 point for A, twice
+    3 points for B, twice
+    2 points for G
+    1 point for E
+
+And to total:
+
+3 + 2*1 + 2*3 + 2 + 1 = 3 + 2 + 6 + 3 = 14
+
+Empty string should return 0. The string can contain spaces and letters (upper and lower case), you should calculate the scrabble score only of the letters in that string.
+
+""           --> 0
+"STREET"    --> 6
+"st re et"    --> 6
+"ca bba g  e" --> 14
+
+ 3 = 14
+
+Empty string should return 0. The string can contain spaces and letters (upper and lower case), you should calculate the scrabble score only of the letters in that string.
+
+""           --> 0
+"STREET"    --> 6
+"st re et"    --> 6
+"ca bba g  e" --> 14 */
+
+  const dict = {
+    1: "AEIOULNRST",
+    2: "DG",
+    3: "BCMP",
+    4: "FHVWY",
+    5: "K",
+    8: "JX",
+    10: "QZ",
+  };
+
+  function scrabbleScore(str) {
+    const s = str.replaceAll(" ", "").toUpperCase();
+    const keys = Object.keys(dict);
+
+    return [...s].reduce(
+      (sum, e) => sum + +keys.find((key) => dict[key].includes(e)),
+      0
+    );
+  }
+
+  console.log(scrabbleScore("cabbage"));
+}
