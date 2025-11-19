14990079e475493161ee289e534f693e8f0b65c6
e4y6 <tchehy9@gmail.com>
2025-11-19 20:22:08 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -1013,7 +1013,48 @@ Empty string should return 0. The string can contain spaces and letters (upper a
 }
 
 {
-  /** */
+  /** 7 kyu  Whose bicycle?
+I am the father of three wonderful sons. before the beginning of the school year, I promised them that I would buy a bicycle for someone who would bring the best marks at the end of the school year. it's time to keep promises and I count on you.
+
+You have 3 input objects(school diaries) with school subjects and marks (1-10). For example:
+
+{
+  'algebra': 6,
+  'history': 8,
+  'physics': 9,
+  'geography': 2,
+  'chemistry': 9
+}
+
+Return please :
+
+'I need to buy a bicycle for my first son.' // the sum of the marks is the highest  in the first diary.
+
+'I need to buy a bicycle for my second son.' // the sum of the marks is the highest in the second diary.
+
+'I need to buy a bicycle for my third son.' //  the sum of the marks is the highest in the third diary.
+
+If two or three sons have the same highest marks, you need to choose the younger one. Use the ageTable object which is preloaded:
+
+ageTable = {
+            'firstSonAge': 14,
+            'secondSonAge': 9,
+            'thirdSonAge': 8
+           }    */
+
+  // function whoseBicycle(diary1, diary2, diary3) {
+  //   const mark = (diary) =>
+  //     Object.values(diary).reduce((res, m) => res + m, 0) /
+  //     Object.values(diary).length;
+  //   const arr = [mark(diary3), mark(diary2), mark(diary1)];
+  //   const i = arr.indexOf(Math.max(...arr));
+
+  //   return i
+  //     ? i > 1
+  //       ? "I need to buy a bicycle for my first son."
+  //       : "I need to buy a bicycle for my second son."
+  //     : "I need to buy a bicycle for my third son.";
+  // }
 
   function whoseBicycle(diary1, diary2, diary3) {
     const mark = (diary) =>
@@ -1022,11 +1063,11 @@ Empty string should return 0. The string can contain spaces and letters (upper a
     const arr = [mark(diary3), mark(diary2), mark(diary1)];
     const i = arr.indexOf(Math.max(...arr));
 
-    return i
-      ? i > 1
-        ? "I need to buy a bicycle for my first son."
-        : "I need to buy a bicycle for my second son."
-      : "I need to buy a bicycle for my third son.";
+    return (
+      "I need to buy a bicycle for my " +
+      (i ? (i > 1 ? "first" : "second") : "third") +
+      " son."
+    );
   }
 
   console.log(
