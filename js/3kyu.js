05f98fe6eb202eb913e61a9525a5ef8d99f49734
e4y6 <tchehy9@gmail.com>
2025-11-21 21:37:38 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -1170,11 +1170,18 @@ In the end, click "Submit" to submit your code pass this kata. */
 }
 
 {
+  // function generatePairs(n) {
+  //   n = Math.trunc(n) + 1;
+  //   return Array.from({ length: n ** 2 })
+  //     .map((e, i) => [Math.floor(i / n), i % n])
+  //     .filter((e) => e[1] >= e[0]);
+  // }
+
   function generatePairs(n) {
-    n = Math.trunc(n) + 1;
-    return Array.from({ length: n ** 2 })
-      .map((e, i) => [Math.floor(i / n), i % n])
-      .filter((e) => e[1] >= e[0]);
+    return ((v) =>
+      Array.from({ length: v ** 2 })
+        .map((e, i) => [Math.floor(i / v), i % v])
+        .filter((e) => e[1] >= e[0]))(Math.trunc(n) + 1);
   }
 
   console.log(generatePairs(2.5));
