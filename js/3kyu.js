615aabeac131264087bed7dab73a6f13f32647f6
e4y6 <tchehy9@gmail.com>
2025-12-14 18:38:26 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -1438,6 +1438,25 @@ A Boolean value (True or False).
 }
 
 {
+  /**  
+7 kyu
+Double Trouble
+
+Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers in the array sum to t. If so, remove the second number.
+
+Example:
+
+x = [1, 2, 3, 4, 5]
+t = 3
+
+1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+
+[1, 3, 4, 5]
+
+Work through the array from left to right.
+
+Return the resulting array.     */
+
   // function trouble(x, t) {
   //   let res = [x[0]];
 
@@ -1459,14 +1478,24 @@ A Boolean value (True or False).
 
   // const trouble = (x, t) => x.reduce((res, e) => (res[res.length - 1] + e !== t ? (res = [...res, e]) : res), []);
 
+  // const trouble = (x, t) => {
+  //   for (let i = 0; i < x.length - 1; i++) {
+  //     if (x[i] + x[i + 1] === t) {
+  //       x.splice(i + 1, 1);
+  //       i--;
+  //     }
+  //   }
+  //   return x;
+  // };
+
   const trouble = (x, t) => {
-    for (let i = 0; i < x.length - 1; i++) {
-      if (x[i] + x[i + 1] === t) {
-        x.splice(i + 1, 1);
-        i--;
+    const res = [x[0]];
+    for (let i = 1; i < x.length; i++) {
+      if (x[i] + res[res.length - 1] !== t) {
+        res.push(x[i]);
       }
     }
-    return x;
+    return res;
   };
 
   // console.log(trouble([4, 1, 1, 1, 4], 2));
