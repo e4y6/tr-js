f7776ccf9ba1e2255795c9719398a447c9ca084b
e4y6 <tchehy9@gmail.com>
2025-12-15 20:59:17 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -1500,3 +1500,47 @@ Return the resulting array.     */
 
   // console.log(trouble([4, 1, 1, 1, 4], 2));
 }
+
+{
+  /** 
+7 kyu
+The Pony Express
+
+A History Lesson
+
+The Pony Express was a mail service operating in the US in 1859-60.
+[Pony Express]
+
+It reduced the time for messages to travel between the Atlantic and Pacific coasts to about 10 days, before it was made obsolete by the transcontinental telegraph.
+How it worked
+
+There were a number of stations, where:
+
+    The rider switched to a fresh horse and carried on, or
+    The mail bag was handed over to the next rider
+
+Kata Task
+
+stations is a list/array of distances (miles) from one station to the next along the Pony Express route.
+
+Implement the riders method/function, to return how many riders are necessary to get the mail from one end to the other.
+
+NOTE: Each rider travels as far as he can, but never more than 100 miles.  */
+
+  function riders(stations) {
+    let res = [];
+    if (stations.length) res.push(1);
+
+    for (let i = 0, sum = 0; i < stations.length; i++) {
+      sum += stations[i];
+      if (sum > 100) {
+        res.push([i]);
+        i--;
+        sum = 0;
+      }
+    }
+    return res.length;
+  }
+
+  console.log(riders([18, 15, 68]));
+}
