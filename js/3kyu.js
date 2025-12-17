ded717dc135c0219e6fb99a2efc71ca3eeda7fd2
e4y6 <tchehy9@gmail.com>
2025-12-17 18:37:29 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -1562,16 +1562,24 @@ NOTE: Each rider travels as far as he can, but never more than 100 miles.  */
   //     [1, 0]
   //   )[0];
 
-  function riders(stations, res = [1]) {
-    for (let i = 0, sum = 0; i < stations.length; i++) {
-      sum += stations[i];
-      if (sum > 100) {
-        res.push([i]);
-        sum = stations[i];
-      }
-    }
-    return res.length;
+  // function riders(stations, res = [1]) {
+  //   for (let i = 0, sum = 0; i < stations.length; i++) {
+  //     sum += stations[i];
+  //     if (sum > 100) {
+  //       res.push([i]);
+  //       sum = stations[i];
+  //     }
+  //   }
+  //   return res.length;
+  // }
+
+  function riders(stations) {
+    return stations.reduce(
+      ([count, dist], s) =>
+        dist + s > 100 ? [(count += 1), s] : [count, dist + s],
+      [1, 0]
+    )[0];
   }
 
-  // console.log(riders([18, 15, 68]));
+  console.log(riders([18, 15, 68]));
 }
