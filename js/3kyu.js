35722d4906026e53338b11ff4c2ffea41a59c0e6
e4y6 <tchehy9@gmail.com>
2026-02-04 11:35:13 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -1686,3 +1686,30 @@ At this level, you don't need validate months and days to calculate the differen
 
   // console.log(logicalCalc([true, true, false, false, true], "XOR"));
 }
+
+{
+  /**  
+7 kyu
+Are they square?
+
+Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+Your function should return true if all elements in the array are square numbers and false if not.
+
+An empty array should return undefined / None / nil /false (for C). You can assume that all array elements will be positive integers.
+
+Examples:
+
+isSquare([1, 4, 9, 16]) --> true
+
+isSquare([3, 4, 7, 9]) --> false
+
+isSquare([]) --> undefined
+ */
+
+  const isSquare = function (arr) {
+    return arr.length ? arr.every((e) => !(e ** 0.5 % 1)) : undefined;
+  };
+
+  console.log(isSquare([1, 25, 9, 576]));
+}
