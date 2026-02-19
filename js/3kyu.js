cd5848548006bf9507e144c9228fae7fd61e7595
e4y6 <tchehy9@gmail.com>
2026-02-19 20:23:32 +0200

    modified:   js/3kyu.js

--- js/3kyu.js
+++ js/3kyu.js
@@ -1795,5 +1795,34 @@ Examples
       ...(input.match(/[aeiou]/g) || []),
     ].join("");
 
-  console.log(moveVowel("apple"));
+  // console.log(moveVowel("apple"));
+}
+
+{
+  /**  
+7 kyu
+String Reversing, Changing case
+Given 2 string parameters, show a concatenation of:
+
+    the reverse of the 2nd string with inverted case; e.g Fish -> HSIf
+    a separator in between both strings: @@@
+    the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER 
+
+** Keep in mind that this kata was initially designed for Shell, I'm aware it may be easier in other languages.
+ */
+
+  function reverseAndMirror(s1, s2) {
+    return (
+      [...s2]
+        .reverse()
+        .map((e) => (e === e.toLowerCase() ? e.toUpperCase() : e.toLowerCase()))
+        .join("") +
+      "@@@" +
+      [...[...s1].reverse(), ...s1]
+        .map((e) => (e === e.toLowerCase() ? e.toUpperCase() : e.toLowerCase()))
+        .join("")
+    );
+  }
+
+  console.log(reverseAndMirror("FizZ", "buZZ"));
 }
